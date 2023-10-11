8b4961e0bde9b51edd770ffbece16124f4214d85
Wanofi Israel <wanofiisrael17@gmail.com>
2023-10-11 10:16:49 +0300

    removed z

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Routes,Route}  from "react-router-dom";
 import App from "./App";
-import Success from "./Components/Success";z
+import Success from "./Components/Success";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
