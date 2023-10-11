a4e9659f58cf45dea946ea6657d3bd2f69ca7477
Wanofi Israel <wanofiisrael17@gmail.com>
2023-10-11 09:47:55 +0300

    remmoved base url

--- src/index.js
+++ src/index.js
@@ -1,16 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter as Router, Routes,Route}  from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route index element={<App/>}/>
-        <Route path="/my_portfolio" element={<App/>}/>
-      </Routes>
+    <Router>
+    <App />
     </Router>
   </React.StrictMode>
 );
